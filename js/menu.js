a8e02e06813d98563cbfaf4683802587c738702b
João Henrique <joaoenrique.san@gmail.com>
2021-10-23 21:46:00 -0300

    Start adaptation to mobile

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,21 @@
 let options = [];
 let menu = document.querySelector('.menu');
 let optionsContainer = menu.querySelector('.menu-options-container');
-
-menu.addEventListener('mouseenter', onMenuHover);
-menu.addEventListener('mouseleave', onMenuClose);
+let isOpen = false;
 
 for (let option of optionsContainer.children){
     options.push(option);
     option.addEventListener('click', toggleSelected);
 }
-
 let selectedOptionIndex = findIndex(document.querySelector('.selected'));
-changeOptionDisplay();
 
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    menu.addEventListener('click', toggleMenuState);
+}else{
+    menu.addEventListener('mouseenter', onMenuHover);
+    menu.addEventListener('mouseleave', onMenuClose);
+}
+changeOptionDisplay();
 function onMenuHover(e){
     if(e.target.classList.contains('menu')){
         optionsContainer.style.marginTop = '0rem';
@@ -27,6 +30,20 @@ function onMenuClose(e){
 }
 function changeOptionDisplay(){
     optionsContainer.style.marginTop = `${9 - 9*selectedOptionIndex}rem`;   
+}
+function toggleMenuState(e){
+    if(e.target.classList.contains('menu') ){
+        if(isOpen){
+            e.target.style.height = '4rem';
+            isOpen = false;
+            changeOptionDisplay();
+        }else{
+            optionsContainer.style.marginTop = '0rem';
+            e.target.style.height = '16rem';
+            isOpen = true;
+        }
+    }
+    e.stopPropagation();
 }
 function toggleSelected(e){
     for(let option of options){
